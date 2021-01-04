999039328629131f0868a8eaeeac1d25c27b8ef7
Piero-V <54007451+Piero-V@users.noreply.github.com>
2021-01-04 15:29:41 -0500

    creacion de la estructura

---
+++ Semana14/Semana14/Interfaces/IConfigDataBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana14.Interfaces
+{
+    class IConfigDataBase
+    {
+    }
+}
---
+++ Semana14/Semana14/Models/Alumnos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana14.Models
+{
+    class Alumnos
+    {
+    }
+}

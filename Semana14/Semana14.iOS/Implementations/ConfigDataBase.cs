a79a859b9e21297149127eb41bbd2f58c820a18f
DarwinSantos <55763569+DarwinSantos@users.noreply.github.com>
2021-01-04 16:58:11 -0500

    Implementation Proyect

---
+++ Semana14/Semana14.iOS/Implementations/ConfigDataBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Semana14.Interfaces;
+using Semana14.iOS.Implementations;
+
+[assembly: Dependency(typeof(ConfigDataBase))]
+namespace Semana14.iOS.Implementations
+{
+    public class ConfigDataBase : IConfigDataBase
+    {
+        public string GetFullPath(string databaseFileName)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", databaseFileName);
+        }
+    }
+}
